f06e9f6d112cec4089479db67afe46369aa917e5
Balazs Nadasdi <yitsushi@gmail.com>
2013-02-21 17:59:17 +0100

    Google+ Beágyazása Octopress-be

---
+++ plugins/googleplus_post_tag.rb
@@ -0,0 +1,162 @@
+# A Liquid tag for Jekyll sites that allows embedding Gists and showing code for non-JavaScript enabled browsers and readers.
+# by: Brandon Tilly
+# Source URL: https://gist.github.com/1027674
+# Post http://brandontilley.com/2011/01/31/gist-tag-for-jekyll.html
+#
+# Example usage: {% gist 1027674 gist_tag.rb %} //embeds a gist for this plugin
+
+require 'cgi'
+require 'digest/md5'
+require 'net/https'
+require 'uri'
+require 'json'
+
+module Jekyll
+  class GooglePLusPostTag < Liquid::Tag
+    def initialize(tag_name, text, token)
+      super
+      @text           = text
+      @cache_folder   = File.expand_path "../.gplus-cache", File.dirname(__FILE__)
+      FileUtils.mkdir_p @cache_folder
+
+      @cache_disabled = false
+    end
+
+    def render(context)
+      if parts = @text.match(/([\d]*) (.*)/)
+        page_id = parts[1].strip
+        post_id = parts[2].strip
+
+        post = get_post(page_id, post_id)
+        #puts post["id"]
+        #script_url = script_url_for gist, file
+        #code       = get_cached_gist(gist, file) || get_gist_from_web(gist, file)
+        #html_output_for script_url, code
+
+        if post["verb"] == "share"
+          return render_share post
+        elsif post["verb"] == "post"
+          return "POST"
+        end
+
+        return "Google+ Embed Fail #{post["verb"]}"
+      else
+        ""
+      end
+    end
+
+    def render_share(post)
+      attachments = []
+      post["object"]["attachments"].each do |staff|
+        if staff["objectType"] == "article"
+          attachments << <<-HTML
+<div class="attachment">
+  <a href="#{staff["url"]}"><img class="left" src="#{staff["fullImage"]["url"]}"></a>
+  <div><a href="#{staff["url"]}"><strong>#{staff["displayName"]}</strong></a></div>
+  <div class="annotation">#{staff["content"]}</div>
+</div>
+          HTML
+        end
+      end
+      <<-HTML
+<div class="googleplusembed">
+  <div class="actor">
+    <img class="left" src="#{post["actor"]["image"]["url"]}">
+    <div>
+      <strong><a href="#{post["actor"]["url"]}">+#{post["actor"]["displayName"]}</a></strong>
+      <small>#{post["published"].gsub(/T.*/, " ")}</small>
+      <a class="jump" href="#{post["url"]}">ugr&aacute;s a bejegyz&eacute;sre</a>
+    </div>
+  </div>
+  <div class="annotation">
+    #{post["annotation"]}
+  </div>
+  <div class="original">
+    <div class="actor">
+      <img class="left" src="#{post["object"]["actor"]["image"]["url"]}">
+      <div>
+        <strong>
+          <a href="#{post["object"]["actor"]["url"]}">+#{post["object"]["actor"]["displayName"]}</a>
+        </strong>
+        <a class="jump" href="#{post["object"]["url"]}">ugr&aacute;s a bejegyz&eacute;sre</a>
+      </div>
+    </div>
+    <div class="annotation">
+      #{post["object"]["content"]}
+    </div>
+    <div class="attachments">#{attachments.join("\n")}</div>
+  </div>
+</div>
+      HTML
+    end
+
+    def cache_post(page_id, post_id, data)
+      cache_file = File.join @cache_folder, "post-#{page_id}-#{post_id}.cache"
+      File.open(cache_file, "w") do |io|
+        io.write data.to_json
+      end
+      data
+    end
+
+    def get_cached_post(page_id, post_id)
+      return nil if @cache_disabled
+      cache_file = File.join @cache_folder, "post-#{page_id}-#{post_id}.cache"
+      File.read cache_file if File.exist? cache_file
+    end
+
+    def get_post(page_id, post_id)
+      post = get_cached_post page_id, post_id
+      return JSON.parse(post) unless post.nil?
+
+      puts "Fetch #{page_id}/#{post_id}"
+      api = GooglePlusAPI.new("AIzaSyD2cOHCHvMs0IbVQnBS13uXmdWWAjh8b14")
+      activities = api.get_activities(page_id)
+      activities["items"].each do |activity|
+        if activity["url"].match /#{page_id}\/posts\/#{post_id}/
+          post = api.get_acivity(activity["id"])
+          cache_post page_id, post_id, post
+        end
+      end
+
+      ""
+    end
+  end
+
+  class GooglePlusAPI
+    def initialize(api_key)
+      @API_KEY  = api_key
+      @base_url = "https://www.googleapis.com/plus/v1"
+    end
+
+    def api_url(path)
+      "#{@base_url}#{path}?key=#{@API_KEY}"
+    end
+
+    def raw_request(path)
+      proxy             = ENV['http_proxy']
+      raw_uri           = URI.parse(api_url(path))
+      if proxy
+        proxy_uri       = URI.parse(proxy)
+        https           = Net::HTTP::Proxy(proxy_uri.host, proxy_uri.port).new raw_uri.host, raw_uri.port
+      else
+        https           = Net::HTTP.new raw_uri.host, raw_uri.port
+      end
+      https.use_ssl     = true
+      request           = Net::HTTP::Get.new raw_uri.request_uri
+      data              = https.request request
+      data              = JSON.parse(data.body)
+
+      data
+    end
+
+    def get_acivity(id)
+      raw_request "/activities/#{id}"
+    end
+
+    def get_activities(id)
+      raw_request "/people/#{id}/activities/public"
+    end
+  end
+end
+
+Liquid::Template.register_tag('googleplus_post', Jekyll::GooglePLusPostTag)
---
+++ source/_posts/2013-02-21-googleplus-beagyazasa-octopress-be.markdown
@@ -0,0 +1,55 @@
+---
+layout: post
+comments: true
+date: 2013-02-21 00:48
+title: Google+ beágyazása Octopress-be
+categories: [ javascript, security, google+, octopress, plugin ]
+image: http://dev.folyam.info.s3.amazonaws.com/2013-02-21-googleplus-beagyazasa-octopress-be/octopress.png
+---
+
+{% img right http://dev.folyam.info.s3.amazonaws.com/2013-02-21-googleplus-beagyazasa-octopress-be/octopress.png "Octopress" %}
+
+Ezzel a bejegyzéssel tesztelem, az [Octopress](http://octopress.org/) kiegészítőmet
+[Google+](https://plus.google.com/)-hoz. A lényege az, hogy a bejegyzésbe beraksz
+egy `googleplus_post` blokkot a bejegyzésbe és a rendszer begenerálja a kívánt tartalmat.
+
+A forrás, amint minden hibáját kigyomláltam publikus lesz,
+ahogyan eddig is minden más. Ha van rá kereslet, akkor akár végig is magyarázom egy cikk
+keretében a működését, illetve miként tudsz te is ilyet írni.
+
+### Működés
+
+Mivel simán beágyazni nem tudom élőben, így a poszt generálásakor egy `Liquid::Tag`
+kiegészítő letölti az adott bejegyzést a Google+ API használatával. A legnagyobb probléma,
+hogy nem lehet értelmesen lekérdezni egy adott bejegyzést, mert a bejegyzés azonosítója
+nem egyenlő az oldalon lévő URL-ben szereplő karakterlánccal. Az egyetlen mód jelenleg amit
+találtam, hogy lekérdezem a bejegyzéseit az adott embernek vagy oldalnak és megnézem
+melyiknek az URL-jeben szerepel a megadott `post_id`. Remélem Azért erre később találok
+értelmes megoldást. Minden megtalált bejegyzés adatát fájlrendszeren elraktározom, hogy
+ne kelljen minden alkalommal megkérdezni a szervert :) Nagyjából ennyi a működése.
+
+<!--more-->
+
+### Használat
+
+    {% raw %}{% googleplus_post page_id post_id %}{% endraw %}
+
+    page_id: Az oldal vagy személy Google+ ID-ja
+    post_id: Az adott bejegyzés ID-ja
+
+Tehát, ha ezt a bejegyzést szeretném megosztani, akkor megnézem ennek az címét, ami nem
+más, mint `https://plus.google.com/u/0/105613682641367710983/posts/DWc2TZgCtuS`.
+
+Ezek alapján `page_id: 105613682641367710983` és `post_id: DWc2TZgCtuS`. Jöhet a cikk és
+berakható `{% raw %}{% googleplus_post 105613682641367710983 DWc2TZgCtuS %}{% endraw %}`
+tag elhelyezéssel.
+
+### Végeredmény
+
+{% googleplus_post 105613682641367710983 DWc2TZgCtuS %}
+
+### Mi a következő lépés?
+
+Kommentek betöltése. Mivel normális embed még nincs, ezért marad az a megoldás jelenleg
+_(mivel mindenféle gányolásos megoldásokat nem szeretem)_, hogy felállítok időkorlátokat.
+Az időkorlátokon belül pedig adott időintervallumban frissítem a megjegyzéseket.
